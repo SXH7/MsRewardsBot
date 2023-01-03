0b2be59c1bc18da82c28aaa7d20695cb13d45c7f
= <=>
2023-01-03 16:27:05 +0530

    browser opens

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.edge.options import Options
 
+options = Options()
+
+driver = webdriver.Edge(options = options)
+driver.get(r"https://youtube.com")
