41367206829323cb2722322e9ec5b3c4ac16f0ce
= <=>
2023-01-10 22:01:20 +0530

    added mobile searches

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ import time
 
 options = Options()
 
+mobile_emulation = {
+     "deviceMetrics": { "width": 375, "height": 812, "pixelRatio": 3.0 },
+     "userAgent" : "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Mobile Safari/537.36 Edg/92.0.902.78"
+}
+
 options.add_argument(r"user-data-dir=C:\Users\Shashwat Singh\AppData\Local\Microsoft\Edge\User Data")
 options.add_argument("profile-directory=Profile 1")
+options.add_experimental_option("mobileEmulation", mobile_emulation)
 options.binary_location = r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
 options.add_experimental_option("detach", True)
 
@@ -19,3 +25,11 @@ while(x<34):
     string = string[:-1]
     driver.get(r"https://www.bing.com/search?q="+string)
     x+=1
+
+stringmob = "mnbvcxzlkjhgfdsapoiut"
+x=0
+while(x<21):
+    driver.get(r"https://www.bing.com/search?q="+stringmob)
+    x+=1
+    stringmob = stringmob[:-1]
+    time.sleep(1)
